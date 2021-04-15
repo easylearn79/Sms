c2e559de0685a935ea21c3bab874df7fa9ae42d0
easylearn76 <azeezdonself@gmail.com>
2021-04-15 04:53:02 -0400

    Merge remote-tracking branch 'github/master'
    # Conflicts:
    #	src/settings.py

--- src/settings.py
+++ src/settings.py
@@ -164,16 +164,20 @@ AWS_STORAGE_BUCKET_NAME = 'stati-files'
 AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
 AWS_S3_OBJECT_PARAMETERS = {'CacheControl': 'max-age=86400'}
 AWS_DEFAULT_ACL = 'public-read'
-
 AWS_LOCATION = 'static'
-#STATICFILES_DIRS = [
- #   os.path.join(BASE_DIR, 'static'),
- #]
-
+# s3 static settings
+STATIC_LOCATION = 'static'
+STATICFILES_STORAGE = 'src.storage_backends.StaticStorage'
 STATIC_URL = 'https://%s/%s/' % (AWS_S3_CUSTOM_DOMAIN, AWS_LOCATION)
-STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
-DEFAULT_FILE_STORAGE = 'core.storages.MediaStore'
-# MEDIA_URL = '/media/'
+DEFAULT_FILE_STORAGE = 'src.storage_backends.StaticStorage'
+
+# s3 public media settings
+PUBLIC_MEDIA_LOCATION = 'media'
+
+MEDIA_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{PUBLIC_MEDIA_LOCATION}/'
+DEFAULT_FILE_STORAGE = 'src.storage_backends.PublicMediaStorage'
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # cloudinary.config(
 # cloud_name =  "dnmgvty4i",

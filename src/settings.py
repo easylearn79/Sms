5b3178563ae8de94ca554f6e45081cb310597036
easylearn76 <azeezdonself@gmail.com>
2021-04-15 11:35:56 -0400

    Initial commit

--- src/settings.py
+++ src/settings.py
@@ -158,26 +158,19 @@ AUTHENTICATION_BACKENDS = ['sms.EmailBackend.EmailBackend']
 # STATIC_URL = '/static/'
 # STATICFILES_DIRS = (os.path.join('static'), )
 
+
 AWS_ACCESS_KEY_ID = 'AKIAYBV3Z5VI4BPROK4Q'
 AWS_SECRET_ACCESS_KEY = '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'
-AWS_STORAGE_BUCKET_NAME = 'stati-files'
-AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
-AWS_S3_OBJECT_PARAMETERS = {'CacheControl': 'max-age=86400'}
-AWS_DEFAULT_ACL = 'public-read'
-AWS_LOCATION = 'static'
-# s3 static settings
-STATIC_LOCATION = 'static'
-STATICFILES_STORAGE = 'src.storage_backends.StaticStorage'
-STATIC_URL = 'https://%s/%s/' % (AWS_S3_CUSTOM_DOMAIN, AWS_LOCATION)
-DEFAULT_FILE_STORAGE = 'src.storage_backends.StaticStorage'
+AWS_STORAGE_BUCKET_NAME =  'stati-files'
 
-# s3 public media settings
-PUBLIC_MEDIA_LOCATION = 'media'
+AWS_S3_FILE_OVERWRITE = False
+AWS_DEFAULT_ACL = None
 
-MEDIA_URL = 'https://%s/%s/' % (AWS_S3_CUSTOM_DOMAIN, PUBLIC_MEDIA_LOCATION)
-DEFAULT_FILE_STORAGE = 'src.storage_backends.PublicMediaStorage'
-MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+
+
+
+
 
 # cloudinary.config(
 # cloud_name =  "dnmgvty4i",
